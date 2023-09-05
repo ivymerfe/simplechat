28d1505594c58a1fa0e2debc17ce73268c05abb5
ivymerfe <82306640+ivymerfe@users.noreply.github.com>
2023-09-05 21:21:08 +0700

    Перешел на postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'khj1dGsf7Ddf',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
